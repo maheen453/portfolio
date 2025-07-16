04f73a503d5841d4faf8fd25131934d0cf81ccc2
maheen <qayyummaheen@gmail.com>
2025-07-16 03:15:49 -0400

    description

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -28,10 +28,10 @@ export const NAVIGATION_LINKS = [
 ];
 
 export const HERO = {
-  name: "CARL FRANK",
-  greet: "Hello there! 👋🏻",
+  name: "Maheen Qayyum",
+  greet: "Hello there!",
   description:
-    "I am a passionate frontend developer with a knack for creating beautiful and functional user interfaces. I like transforming ideas into engaging web experiences for over a decade.",
+    "",
 };
 
 export const PROJECTS = [
